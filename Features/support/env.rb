e25aafcfac107702a813d0933a8735e9833aeadc
keith.ford1980@googlemail.com <b1lly1sg0d>
2013-05-20 22:24:59 +0100

    env.rb clean up

--- Features/support/env.rb
+++ Features/support/env.rb
@@ -5,36 +5,11 @@ require 'rspec/expectations'
 require 'watir-webdriver-performance'
 
 include Test::Unit::Assertions
-#require 'synchronize'
-#require 'debugger'
-#require 'gherkin'
-#require 'features/support/cucumber_settings'
 
-#config to run in firefox with clean profile
-#profile = Selenium::WebDriver::Firefox::Profile.new
-#browser = Watir::Browser.new :firefox, :profile => profile
-
-#config to run ie - BEWARE THIS IS SLOW, ONLY REALLY GOOD FOR OVERNIGHT
-#browser = Watir::Browser.new :ie
 
 Before do
 
-   #require 'features/support/cucumber_settings'
 
-
-  #ENV['@base_url'] = LOCALURL
-  #ENV['@base_url2'] = LOCALADD
-  #@base_url2 = LOCALADD
-  #@base_url = LOCALURL
-  #if ENV['ENVIRONMENT'] == "auto"
-  #  ENV['@base_url'] = AUTOURL
-  #  ENV['@base_url2'] = AUTOADD
-   ## @base_url = AUTOURL
-   # @base_url2 = AUTOADD
-  #end
-
-  #@browser = browser
-  #@results_table_timeout = RESULTS_TABLE_TIMEOUT
 end
 
 
